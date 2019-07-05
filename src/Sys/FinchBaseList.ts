169c565e3db5319ed421ff97c9cc88b3af1e3bd3
elex <elextraza@gmail.com>
2019-07-05 17:21:28 +0400

    Параметры для базового списка

--- src/Sys/FinchBaseList.ts
+++ src/Sys/FinchBaseList.ts
@@ -1,19 +1,44 @@
 import FinchBase from './FinchBase';
 import ErrorSys from './ErrorSys';
 
+enum Sort {
+    asc =  'asc',
+    desc = 'desc'
+}
+
 /**
  * Базовый список
  */
 export default class FinchBaseList {
 
+    public total: number; // общее число объектов в БД
+
     protected db: any; // драйвер DB
 
     public errorSys: ErrorSys; // обработчик ошибок
+
+    public offset: number;
+    public limit: number;
+
+    public sort: Sort;
+    public sortField: string;
+
+    constructor() {
+        this.errorSys = new ErrorSys();
+        this.total = 0;
+
+        this.offset = 0;
+        this.limit = 20;
+        this.sort = Sort.asc;
+        this.sortField = 'id';
+    }
+
 
     /**
      * Список объектов
      */
     public async list(): Promise<FinchBase[]> {
+        this.total = 0;
         return [];
     }
 
@@ -23,7 +48,7 @@ export default class FinchBaseList {
      * @param id[] - список id для синхронизации
      */
     public async sync(id: number[]): Promise<FinchBase[]> {       
-
+        this.total = 0;
         return [];
     }
 
